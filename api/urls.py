5b033fb4d213ecb40ecd454455f5fb7c9b811ead
Alex Maldonado <alex@andrewblankenship.dev>
2024-07-19 20:55:14 -0500

    combine backends

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,17 @@
 
 from django.urls import path
 from .views import AccomplishmentListView, AccomplishmentDeleteView
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def test_view(request):
+    return HttpResponse("Test view working")
 
 urlpatterns = [
     path('accomplishments/', AccomplishmentListView.as_view(), name='accomplishments-list'),
     path('accomplishments/delete/<int:pk>/', AccomplishmentDeleteView.as_view(), name='accomplishment-delete'),
+    path('', test_view, name='test'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
