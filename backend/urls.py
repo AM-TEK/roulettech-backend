cbca3a8b6f6e92519e2abe6a8ceb7fe16b4c7666
Alex Maldonado <alex@andrewblankenship.dev>
2024-07-19 20:47:43 -0500

    test view 2

--- backend/urls.py
+++ backend/urls.py
@@ -20,26 +20,43 @@
 #     path('api/', include('api.urls')),
 #     path('', home, name='home'),
 # ]
-from django.contrib import admin
-from django.urls import path, include
-from api.views import CreateUserView, api_root
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
+
+
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from api.views import CreateUserView, api_root
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+
+# from django.http import HttpResponse
+
+# def test_view(request):
+#     return HttpResponse("Test view working")
+
+
+# urlpatterns = [
+#     path('', api_root, name='api-root'),
+#     path('admin/', admin.site.urls),
+#     path('api/user/register/', CreateUserView.as_view(), name='register'),
+#     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+#     path('api/auth/', include('rest_framework.urls')),
+#     path('api/', include('api.urls')),
+# ]
+
+
+from django.contrib import admin
+from django.urls import path
 from django.http import HttpResponse
 
 def test_view(request):
     return HttpResponse("Test view working")
 
-
 urlpatterns = [
-    path('', api_root, name='api-root'),
+    path('', test_view, name='test'),
     path('admin/', admin.site.urls),
-    path('api/user/register/', CreateUserView.as_view(), name='register'),
-    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
-    path('api/auth/', include('rest_framework.urls')),
-    path('api/', include('api.urls')),
 ]
